cc06737b0c9febb927b0573a598693a86bba308e
Yasin Yusuf <yasinyus@users.noreply.github.com>
2025-09-02 12:47:54 +0700

    add port

--- index.js
+++ index.js
@@ -45,6 +45,6 @@ app.use('/volume', volumeTabungRoutes);
 app.use('/audit', auditRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
